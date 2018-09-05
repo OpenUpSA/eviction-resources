367cedcd4045e3a677c9f6b0522cee1a87681ad7
Schalk Venter <venterschalk@gmail.com>
2018-09-05 20:44:45 +0200

    Adds dynamic genereated completed percentage

--- src/components/AffidavitsList/index.jsx
+++ src/components/AffidavitsList/index.jsx
@@ -1,8 +1,42 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import AffidavitPreview from './../AffidavitPreview';
 import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
 import Typography from '@material-ui/core/Typography';
+import { mean } from 'lodash';
+
+
+function determinePercentageCompleted(object) {
+  const completed = Object.keys(object).reduce(
+    (result, key) => {
+      if (
+        (
+          typeof object[key] === 'string' &&
+          object[key] === ''
+        ) ||
+        (
+          Array.isArray(object[key]) &&
+          object[key].length < 1
+        )
+      ) {
+        return result;
+      }
+      
+      return result + 1;
+    },
+    0
+  )
+
+  const length = Object.keys(object).length;
+  return Math.floor((completed / length) * 100);
+}
+
+function calcEntireAverageCompleted(affidavitId, affidavits, people) {
+  const affidavit = affidavits[affidavitId]
+  const array = affidavit.people.map(id => determinePercentageCompleted(people[id]))
+  const meanResult = mean(array);
+  console.log(affidavit, array, meanResult, affidavit.people.length)
+  return affidavit.people.length === 1 ? Math.floor(meanResult / 3) : meanResult;
+}
 
 
 function AffidavitsList({ affidavits, people, itemClickCallback }) {
@@ -28,10 +62,11 @@ function AffidavitsList({ affidavits, people, itemClickCallback }) {
           const displayName = hasFirstAndLastNames ? `${firstName} ${lastName}` : 'Unknown Person'
           const occupants = Object.keys(affidavits[key].people).length;
           const date = new Date(affidavits[key].created);
+          const completed = calcEntireAverageCompleted(key, affidavits, people);
 
           return (
             <div style={{ margin: '0.5rem' }} key={key} onClick={() => itemClickCallback && itemClickCallback(key)}>
-              <AffidavitPreview name={displayName} occupants={occupants} date={date} />
+              <AffidavitPreview name={displayName} occupants={occupants} date={date} completed={completed} />
             </div>
           )
         })
